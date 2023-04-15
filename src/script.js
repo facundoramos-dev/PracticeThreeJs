37635174590825c5562fbd4303c41dc5b4a7dbdd
seba1499 <faq.ramos.14101999@gmail.com>
2023-04-15 14:40:21 -0500

    animations with gsap add

--- src/script.js
+++ src/script.js
@@ -3,8 +3,9 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 import * as dat from "lil-gui";
+import { gsap } from "gsap/all";
 
-// const gui = new dat.GUI();
+const gui = new dat.GUI();
 
 const canvas = document.querySelector("canvas.webgl");
 
@@ -15,9 +16,21 @@ const matcapTexture = textureLoader.load("textures/matcaps/8.png");
 
 const fontLoader = new FontLoader();
 
+// const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+// const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
+// const donut = new THREE.Mesh(donutGeometry, material);
+// scene.add(donut);
+
+// console.log("donut: ", donut.position);
+// gsap.to(donut.rotation, {
+//   x: Math.PI,
+//   duration: 5,
+//   repeat: -1,
+//   ease: "none",
+// });
+
 fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
-  const textGeometry = new TextGeometry("mmm... cereal", {
+  const textConfig = {
     font: font,
     size: 0.5,
     height: 0.2,
@@ -27,24 +40,90 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     bevelSize: 0.02,
     bevelOffset: 0,
     bevelSegments: 5,
-  });
-  textGeometry.center();
+  };
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  const textGeometry1 = new TextGeometry("mmm   cereal", textConfig);
+  const textGeometry2 = new TextGeometry(".", textConfig);
 
-  const text = new THREE.Mesh(textGeometry, material);
-  scene.add(text);
+  textGeometry1.computeBoundingBox();
+  textGeometry2.computeBoundingBox();
+
+  textGeometry1.center();
+  textGeometry2.center();
+
+  const text1 = new THREE.Mesh(textGeometry1, material);
+  const text2 = new THREE.Mesh(textGeometry2, material);
+  const text3 = new THREE.Mesh(textGeometry2, material);
+  const text4 = new THREE.Mesh(textGeometry2, material);
+  const group = new THREE.Group();
+  group.add(text1, text2, text3, text4);
+
+  const scale = 1.2;
+  // gsap.to(text.scale, {
+  //   x: scale,
+  //   y: scale,
+  //   z: scale,
+  //   duration: 5,
+  //   repeat: -1,
+  //   yoyo: true,
+  // });
+  const box = textGeometry2.boundingBox.max.x * 2;
+  console.log("GEOM", textGeometry1.boundingBox.max);
+
+  text2.position.x = -box / 2;
+  text3.position.x = -box * 1.75;
+  text4.position.x = -box * 3;
+
+  const boxY =
+    textGeometry1.boundingBox.max.y -
+    (textGeometry2.boundingBox.max.y - 0.021) * 2;
+  text2.position.y = -boxY;
+  text3.position.y = -boxY;
+  text4.position.y = -boxY;
+  // gui.add(text2.position, "y", -box * 4, box, box);
+  // gui.add(text3.position, "y", -box * 4, box, box);
+  // gui.add(text4.position, "y", -box * 4, box, box);
+  console.log("group: ", group);
+
+  scene.add(group);
+
+  const tl = gsap.timeline({
+    defaults: { duration: 0.5 },
+    repeat: -1,
+    yoyo: true,
+  });
+  tl.fromTo(text4.scale, { x: 0, y: 0 }, { x: 1, y: 1 })
+    .fromTo(text3.scale, { x: 0, y: 0 }, { x: 1, y: 1 }, ">")
+    .fromTo(text2.scale, { x: 0, y: 0 }, { x: 1, y: 1 }, ">")
+    .fromTo(
+      group.scale,
+      { x: 1, y: 1, z: 1 },
+      { x: 1.05, y: 1.05, z: 1.05, duration: 2 },
+      ">"
+    );
 
   const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
+
+  const min = 5,
+    max = 7;
 
   for (let i = 0; i < 100; i++) {
     const donut = new THREE.Mesh(donutGeometry, material);
     donut.position.x = (Math.random() - 0.5) * 10;
     donut.position.y = (Math.random() - 0.5) * 10;
     donut.position.z = (Math.random() - 0.5) * 10;
-    donut.rotation.x = Math.random() * Math.PI;
+    // donut.rotation.x = Math.random() * Math.PI;
     donut.rotation.y = Math.random() * Math.PI;
     const scale = Math.random();
     donut.scale.set(scale, scale, scale);
 
+    let time = Math.floor(Math.random() * (max - min) + min);
+    gsap.to(donut.rotation, {
+      x: Math.PI * 2,
+      duration: time,
+      repeat: -1,
+      ease: "none",
+    });
     scene.add(donut);
   }
 });
